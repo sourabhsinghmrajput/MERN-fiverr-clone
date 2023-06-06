6efb7bb360e9a738893ec91f0e8dc1fa5eb0ee99
sourabhsinghmrajput <87462100+sourabhsinghmrajput@users.noreply.github.com>
2023-06-06 19:14:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Home from "./pages/home/Home";
+import Gig from "./pages/gig/Gig";
+import Gigs from "./pages/gigs/Gigs";
+import MyGigs from "./pages/myGigs/MyGigs";
+import Add from "./pages/add/Add";
+import Message from "./pages/message/Message";
+import Messages from "./pages/messages/Messages";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Order from "./pages/order/Order";
+import "./App.scss";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+function App() {
+  const Layout = () => {
+    return (
+      <>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/gig/:id",
+          element: <Gig />,
+        },
+        {
+          path: "/order",
+          element: <Order />,
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/add",
+          element: <Add />,
+        },
+        {
+          path: "/messages",
+          element: <Messages />,
+        },
+        {
+          path: "/message/:id",
+          element: <Message />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
